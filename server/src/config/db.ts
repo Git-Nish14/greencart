6eb9fe48d962e0fa7992a7d5b0eeefa87f6e8207
Nish Patel <nish14062003@gmail.com>
2025-04-23 14:59:43 +0530

    database connected

---
+++ server/src/config/db.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    mongoose.connection.on("connected", () =>
+      console.log("Database Connected")
+    );
+    await mongoose.connect(`${process.env.MONGODB_URL}`);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    } else {
+      console.error("An unknown error occurred");
+    }
+  }
+};
+
+export default connectDB;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,35 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import connectDB from "./config/db";
+import "dotenv/config";
+import userRouter from "./routes/userRoute";
 
 const app = express();
 const port = process.env.PORT || 4000;
 
+const allowedOrigins = ["http://localhost:3000"];
+
+// Middleware configuration
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: allowedOrigins, credentials: true }));
+
 app.get("/", (req, res) => {
   res.send("API is Working");
 });
+app.use("/api/user", userRouter);
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+async function startServer() {
+  try {
+    await connectDB();
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error);
+    process.exit(1);
+  }
+}
+
+startServer();
